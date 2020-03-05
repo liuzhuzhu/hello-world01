440b1cb3e65969e0d495d9f5517ddf0117fbc62f
liuhongda <liuhondga@atguigu.com>
2020-03-05 11:33:44 +0800

    测试jenkins第一步

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="http://5b0988e595225.cdn.sohucs.com/images/20181022/1b01954eda7d4623b81d29010d4d6e64.jpeg"><br/>
+    <h2>永远相信美好的事情即将发生</h2><br/>
+    <h2>我爱小米！！！</h2><br/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ start.sh
@@ -0,0 +1,16 @@
+# 拉取代码
+# git pull
+
+# 安装依赖和打包
+yarn --registry=https://registry.npm.taobao.org/ && yarn build
+
+# 删除容器
+docker rm -f demo1 &> /dev/null
+
+#启动容器
+docker run -d --restart=on-failure:5\
+    -p 8080:80 \
+    -v $PWD/dist:/usr/share/nginx/html \
+    --name demo1 nginx
+
+
