a5249a42be789bc04837d461af0dcae1401f7834
liuhongda <liuhondga@atguigu.com>
2020-03-05 21:01:26 +0800

    我爱徐二狗

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <img src="http://5b0988e595225.cdn.sohucs.com/images/20181022/1b01954eda7d4623b81d29010d4d6e64.jpeg"><br/>
     <h2>永远相信美好的事情即将发生</h2><br/>
     <h2>我爱小米！！！</h2><br/>
+    <h2>我爱徐二狗</h2>
   </div>
 </template>
 
