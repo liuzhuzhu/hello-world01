fbcabfe76e4c77b31a8809d52689b8ed3a395940
liuhongda <liuhondga@atguigu.com>
2020-03-10 11:15:23 +0800

    测试脚本

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <img src="https://gss3.bdstatic.com/84oSdTum2Q5BphGlnYG/timg?wapp&quality=80&size=b150_150&subsize=20480&cut_x=0&cut_w=0&cut_y=0&cut_h=0&sec=1369815402&srctrace&di=f4c99e3b274448207b811f3da3294fe6&wh_rate=null&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fpic%2Fitem%2Fd7650ac2d56285354a41a70398ef76c6a7ef6320.jpg"><br/>
     <h2>永远相信美好的事情即将发生</h2><br/>
     <h2>锤子手机</h2><br/>
+    <h2>中国加油，武汉加油～～</h2>
   </div>
 </template>
 
