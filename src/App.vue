90e93a328c3884bb6584329e650ae2f3f505b635
liuzhuzhu <846061407@qq.com>
2020-09-16 10:26:41 +0800

    meba out

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
-    <h2>hello 0316</h2>
-    <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597570003269&di=112e1663da6ae0f089aa8f519d489c67&imgtype=0&src=http%3A%2F%2F03imgmini.eastday.com%2Fmobile%2F20181103%2F20181103152313_17aeb85b472e5c85f4753c21452e4939_2.jpeg"><br/>
-    <img src = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597570003267&di=be2fe69e52597199ddbc1d5588f7fa63&imgtype=0&src=http%3A%2F%2Fu.candou.com%2Fs%2F500%2F2018%2F1104%2F1541302510524.jpeg"/>
-    <p>
-      唐纳德·特朗普（Donald Trump，1946年6月14日- ），出生于美国纽约，美国共和党籍政治家、企业家、房地产商人、电视人，第45任美国总统。
-      特朗普于1968年获得宾夕法尼亚大学沃顿商学院经济学学士学位，随后任职于父亲的房地产公司。1971年接管公司，从事房地产开发，投资范围逐步延伸至其他多个行业。特朗普于2015年6月以美国共和党人身份宣布参选美国总统，
-      2016年11月9日当选美国第45任总统，2017年1月20日宣誓就职。2019年9月24日，美国国会众议院启动针对特朗普的弹劾调查，12月18日经过全体投票通过两项弹劾条款。2020年2月5日，美国国会参议院否决了两项弹劾条款，弹劾失败
-    </p>
+    <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2244407442,3539065501&fm=26&gp=0.jpg"/>
   </div>
 </template>
 
