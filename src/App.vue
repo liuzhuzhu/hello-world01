0cee8495deaaf51c68d3c79108df9a01fb2f98bc
liuzhuzhu <846061407@qq.com>
2020-09-16 11:35:35 +0800

    hi friend

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h2>预祝大家 都能知道自己心仪的 另一半</h2>
     <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2244407442,3539065501&fm=26&gp=0.jpg"/>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600233697591&di=0975e6c8f4f41586bb6451674d13ed77&imgtype=0&src=http%3A%2F%2Fimg0.imgtn.bdimg.com%2Fit%2Fu%3D2681546830%2C1272261180%26fm%3D214%26gp%3D0.jpg"/>
   </div>
