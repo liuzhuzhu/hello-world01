8048fa5401fff70e52ce5b6b92a27a3dec4c2b66
liuzhuzhu <846061407@qq.com>
2020-11-25 16:07:37 +0800

    马老师，好自为之 前程似锦

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h2>预祝0522java全体学生前程似锦</h2>
+      <h2>预祝0522java全体学生前程似锦</h2>
    <img src="https://bkimg.cdn.bcebos.com/pic/c75c10385343fbf2b211d073b737dd8065380cd72100?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UxODA=,g_7,xp_5,yp_5"/>
    <div>
      2001年，马保国陪儿子马晓阳到英国读书，因缺学费开始在英国开馆授徒，在英国授徒后，马保国自称被英国武术界誉为“功夫王”“当代李小龙”。 [1]
