c0f8b9ca399bc5ed332fa6fc1db1387e14fd3267
liuzhuzhu <846061407@qq.com>
2020-11-25 10:45:46 +0800

    0522 第一天

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h2>预祝大家前程似锦</h2>
+    <h2>预祝0522java全体学生前程似锦</h2>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603448369683&di=2b57cbc2aefb46e98d289d30e681445a&imgtype=0&src=http%3A%2F%2F07imgmini.eastday.com%2Fmobile%2F20200928%2F20200928190700_9b19db8ba762bb449ac70a49ceae8e60_19.jpeg"/>
     <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603444352323&di=cb91bede6cfa10c179877910bd27847e&imgtype=0&src=http%3A%2F%2Fp0.itc.cn%2Fimages01%2F20201019%2F444b2ec6d9464f29ab8f94a0b21ca5cb.jpeg"/>
   </div>
