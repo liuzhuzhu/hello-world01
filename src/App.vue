e8edd582e8b8b72de8e6af52ea4c3ac0ce9e9f5d
liuzhuzhu <846061407@qq.com>
2021-01-12 14:20:18 +0800

    修复文字和图片---演示脚本

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h2>预祝0713java全体学生前程似锦</h2>
-   <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180320%2Fa975cae569db4ff8b812461f2323260a.jpeg&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613023632&t=444398adfd706a51f2da15ad2146e49b"/>
+   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
 
