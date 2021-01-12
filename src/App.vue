57d00e2ed0db5e69457e831a4b2e7639e761ff7c
liuzhuzhu <846061407@qq.com>
2021-01-12 15:11:29 +0800

    测试自动化部署---中测

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
       <h2>预祝0713java全体学生前程似锦</h2>
       <h2>预祝0713java全体学生统统月薪过万</h2>
+      <h2>预祝0713java全体学生统统月薪过两万</h2>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
