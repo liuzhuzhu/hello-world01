768e45bcee2f2ab60108e3726ad5213a646a799a
liuzhuzhu <846061407@qq.com>
2021-03-02 14:40:28 +0800

    测试手动部署

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+      <p>java0821 全体同学 前程似锦</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
