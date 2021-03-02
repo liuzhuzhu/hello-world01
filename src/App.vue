42eea505a31add0d6ae6421c92761faa5782ed68
liuzhuzhu <846061407@qq.com>
2021-03-02 14:48:09 +0800

    貌美如花

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
       <p>java0821 全体同学 前程似锦</p>
+      <p>java0821 全体同学 貌美如花</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
