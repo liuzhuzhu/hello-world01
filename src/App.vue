0910528491ff421df13709eaba4ca21412781cad
liuzhuzhu <846061407@qq.com>
2021-03-02 18:14:17 +0800

    测试自动化部署2

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <div id="app">
       <p>java0821 全体同学 前程似锦</p>
       <p>java0821 全体同学 貌美如花</p>
-      <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
-      <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
