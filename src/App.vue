39b44fefed6e2305db9525bf43d4876f8126ad21
liuzhuzhu <846061407@qq.com>
2021-04-02 11:48:49 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
       <p>java0923 全体同学 行走的offer收割机</p>
-      <p>java0923 全体同学 貌美如花</p>
+      <p>java0923 全体同学 争取第一周入职 第二周转正,第三周升为组长,第四周董事会</p>
       <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
       <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
