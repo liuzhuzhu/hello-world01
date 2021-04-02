d8f6c540c265b981ad1bf9e500ad0db78b4fcdd3
liuzhuzhu <846061407@qq.com>
2021-04-02 14:40:08 +0800

    一张小小的图片

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <p>java0923 全体同学 争取第一周入职 第二周转正,第三周升为组长,第四周董事会</p>
       <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
       <p>祝 java0923 全体同学 拿到自己心仪的offer</p>
-   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+   <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3101694723,748884042&fm=26&gp=0.jpg"/>
   </div>
 </template>
 
